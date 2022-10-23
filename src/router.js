a7f19702556361daaabc7963179c2e74d99d31a9
dayana-pv <dayana.priscilla.17@gmail.com>
2022-10-22 19:44:06 -0500

    Se añadio la autenticacion con camara

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { Navigate, useRoutes } from "react-router-dom";
 import Layout from "./layout/Layout";
 import DocumentPage from "./pages/DocumentPage";
 import ValidateLogin from "./pages/ValidateLogin";
+import AuthenticationLogin from "./pages/AuthenticationLogin";
 
 export const AppRouter = () => {
   return useRoutes([
@@ -16,6 +17,10 @@ export const AppRouter = () => {
         {
           path: "validate-login",
           element: <ValidateLogin />,
+        },
+        {
+          path: "authentication-login",
+          element: <AuthenticationLogin />,
         },
         { path: "", element: <Navigate to="/document-login" /> },
       ],
